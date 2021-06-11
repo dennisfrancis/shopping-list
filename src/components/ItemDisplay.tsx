100475f0aeca9d60e55a446730b86f71d287eeac
Dennis Francis <dennisfrancis.in@gmail.com>
2021-06-11 19:07:52 +0530

    highlight selected item

--- src/components/ItemDisplay.tsx
+++ src/components/ItemDisplay.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import { Item, ItemStatesAndSetters } from "../types/item";
 
 export function ItemDisplay(props: {
     item: Item,
     newItemStatesAndSetters: ItemStatesAndSetters
 }) {
+    const itemStyle: React.CSSProperties = {display: 'flex', flexDirection: 'row', justifyContent: 'space-between'};
+
+    itemStyle.background = (props.newItemStatesAndSetters.name === props.item.name) ? '#00bfff' : '#ffffff';
+
     const handleItemClick = () => {
         props.newItemStatesAndSetters.setName(props.item.name);
         props.newItemStatesAndSetters.setQuantity(props.item.quantity);
@@ -12,7 +17,7 @@ export function ItemDisplay(props: {
         props.newItemStatesAndSetters.setExisting(true);
     };
     return (
-        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}} onClick={handleItemClick}>
+        <div style={itemStyle} onClick={handleItemClick}>
             <div>{props.item.name}</div>
             { props.item.comment && <div>({props.item.comment})</div> }
             <div>{props.item.quantity}&nbsp;{props.item.unit}</div>
