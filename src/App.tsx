cbbc27f36ffd579d0c26feca3a522b7dcbad90a7
Dennis Francis <dennisfrancis.in@gmail.com>
2021-06-19 12:45:37 +0530

    move the states to top level App
    This lets us share the state between other future routes like /previous
    and avoid re-fetches on switching routes.

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ import {
 } from "react-router-dom";
 
 import './bootstrap.min.css';
+import { useState } from "react";
+import { Item, ItemStatesAndSetters } from './types/item';
+import { useEffect } from "react";
+
 
 import { NewList } from './pages/NewList';
 import UserData from './pages/UserData';
@@ -23,6 +27,73 @@ let storage = new StorageType();
 })();
 
 function App() {
+  let [masterList, setMasterList] = useState(new Set<string>([]));
+  let [dbIsEmpty, setDBIsEmpty] = useState(false);
+  let [masterItems, setMasterItems] = useState([] as Item[]);
+  let [newList, setNewList] = useState<Item[]>([]);
+  let [name, setName] = useState('');
+  let [quantity, setQuantity] = useState(0);
+  let [unit, setUnit] = useState('');
+  let [comment, setComment] = useState('');
+  let [date, setDate] = useState(new Date());
+  let [existing, setExisting] = useState(false);
+  let [runFetchEffect, setRunFetchEffect] = useState(false);
+  let itemStatesAndSetters: ItemStatesAndSetters = {
+      name,
+      setName,
+      quantity,
+      setQuantity,
+      unit,
+      setUnit,
+      comment,
+      setComment,
+      existing,
+      setExisting,
+      date,
+      setDate
+  };
+
+  // fetch initial state from DB.
+  useEffect(() => {
+      if (!runFetchEffect && (masterList.size || dbIsEmpty)) {
+          return;
+      }
+
+      if (storage.hasDB()) {
+          // console.log('Fetching...');
+          storage.fetch();
+      }
+
+      const dbListener = (mList: Item[]) => {
+          if (runFetchEffect)
+              setRunFetchEffect(false);
+          setDBIsEmpty((mList.length === 0));
+          setMasterList(new Set<string>(mList.map(item => item.name)));
+          const nameToItem = new Map<string, Item>();
+          const currentList: Item[] = [];
+          mList.forEach((item) => {
+              if (!item.saved)
+                  currentList.push(item);
+
+              let prevItem = nameToItem.get(item.name);
+              if (!prevItem)
+                  nameToItem.set(item.name, item);
+              else if (prevItem.date < item.date)
+                  nameToItem.set(item.name, item);
+          });
+          setMasterItems([...nameToItem.values()]);
+          setNewList(currentList);
+          if (currentList.length) {
+              setDate(currentList[0].date);
+          }
+      };
+      storage.addListener(dbListener);
+
+      return function cleanup() {
+          storage.removeListener(dbListener);
+      };
+  });
+
   return (
     <Router>
       <div>
@@ -45,7 +116,10 @@ function App() {
               <UserData />
             </Route>
             <Route path="/">
-                <NewList />
+                <NewList masterList={masterList} setMasterList={setMasterList}
+                  newList={newList} setNewList={setNewList}
+                  masterItems={masterItems} newItemStatesAndSetters={itemStatesAndSetters}
+                  runFetchEffect={runFetchEffect} setRunFetchEffect={setRunFetchEffect}/>
             </Route>
           </Switch>
         </StorageProvider>
--- src/pages/NewList.tsx
+++ src/pages/NewList.tsx
@@ -1,86 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 import { Item, ItemStatesAndSetters } from '../types/item';
 import { ItemControls } from "../components/ItemControls";
 import { DebugItemLists } from "../components/DebugItemLists";
 import { ItemList } from '../components/ItemList';
 import { useContext } from "react";
 import { StorageContext } from "../contexts/storage";
-import { useEffect } from "react";
 
-export function NewList() {
-    let [masterList, setMasterList] = useState(new Set<string>([]));
-    let [dbIsEmpty, setDBIsEmpty] = useState(false);
-    let [masterItems, setMasterItems] = useState([] as Item[]);
-    let [newList, setNewList] = useState<Item[]>([]);
-    let [name, setName] = useState('');
-    let [quantity, setQuantity] = useState(0);
-    let [unit, setUnit] = useState('');
-    let [comment, setComment] = useState('');
-    let [date, setDate] = useState(new Date());
-    let [existing, setExisting] = useState(false);
-    let [runFetchEffect, setRunFetchEffect] = useState(false);
+export function NewList(props: {
+    masterList: Set<string>;
+    masterItems: Item[];
+    setMasterList: React.Dispatch<React.SetStateAction<Set<string>>>;
+    newList: Item[];
+    setNewList: React.Dispatch<React.SetStateAction<Item[]>>;
+    newItemStatesAndSetters: ItemStatesAndSetters;
+    runFetchEffect: boolean;
+    setRunFetchEffect: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
     const storage = useContext(StorageContext);
-    let itemStatesAndSetters: ItemStatesAndSetters = {
+    let {
         name,
         setName,
-        quantity,
         setQuantity,
-        unit,
         setUnit,
-        comment,
         setComment,
-        existing,
-        setExisting,
-        date,
-        setDate
-    };
-
-    // fetch initial state from DB.
-    useEffect(() => {
-        if (!runFetchEffect && (masterList.size || dbIsEmpty)) {
-            return;
-        }
-
-        if (storage.hasDB()) {
-            // console.log('Fetching...');
-            storage.fetch();
-        }
-
-        const dbListener = (mList: Item[]) => {
-            if (runFetchEffect)
-                setRunFetchEffect(false);
-            setDBIsEmpty((mList.length === 0));
-            setMasterList(new Set<string>(mList.map(item => item.name)));
-            const nameToItem = new Map<string, Item>();
-            const currentList: Item[] = [];
-            mList.forEach((item) => {
-                if (!item.saved)
-                    currentList.push(item);
-
-                let prevItem = nameToItem.get(item.name);
-                if (!prevItem)
-                    nameToItem.set(item.name, item);
-                else if (prevItem.date < item.date)
-                    nameToItem.set(item.name, item);
-            });
-            setMasterItems([...nameToItem.values()]);
-            setNewList(currentList);
-            if (currentList.length) {
-                setDate(currentList[0].date);
-            }
-        };
-        storage.addListener(dbListener);
-
-        return function cleanup() {
-            storage.removeListener(dbListener);
-        };
-    });
+        setExisting
+    } = props.newItemStatesAndSetters;
 
     const removeItem = (item: Item) => {
         const nameToRemove = item.name;
-        const list = newList.filter(oldItem => oldItem.name !== nameToRemove);
+        const list = props.newList.filter(oldItem => oldItem.name !== nameToRemove);
         storage.delete(item);
-        setNewList(list);
+        props.setNewList(list);
         if (name === nameToRemove) {
             setName('');
             setQuantity(0);
@@ -94,12 +44,12 @@ export function NewList() {
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', alignItems: "flex-start"}}>
-            <ItemControls masterList={masterList} setMasterList={setMasterList}
-                newList={newList} setNewList={setNewList}
-                masterItems={masterItems} newItemStatesAndSetters={itemStatesAndSetters}/>
-            <ItemList newList={newList} newItemStatesAndSetters={itemStatesAndSetters}
-                removeItem={removeItem} setRunFetchEffect={setRunFetchEffect}/>
-            {debugMode && <DebugItemLists masterList={masterList} newList={newList} />}
+            <ItemControls masterList={props.masterList} setMasterList={props.setMasterList}
+                newList={props.newList} setNewList={props.setNewList}
+                masterItems={props.masterItems} newItemStatesAndSetters={props.newItemStatesAndSetters}/>
+            <ItemList newList={props.newList} newItemStatesAndSetters={props.newItemStatesAndSetters}
+                removeItem={removeItem} setRunFetchEffect={props.setRunFetchEffect}/>
+            {debugMode && <DebugItemLists masterList={props.masterList} newList={props.newList} />}
         </div>
     );
 }
