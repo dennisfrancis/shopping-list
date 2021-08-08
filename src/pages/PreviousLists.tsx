942a3d0ee044779ea1b74b58cfdf1c1adc887988
Dennis Francis <dennisfrancis.in@gmail.com>
2021-08-08 16:36:38 +0530

    improve small screen ui for previous lists

--- src/pages/PreviousLists.tsx
+++ src/pages/PreviousLists.tsx
@@ -4,6 +4,8 @@ import { ListDisplay  } from "../components/ListDisplay";
 import { ItemList } from "../components/ItemList";
 import { useState } from "react";
 
+import "../styles/previouslists.css";
+
 export function PreviousLists(props: {
     dateMap: Map<number, Item[]>;
 }) {
@@ -11,17 +13,17 @@ export function PreviousLists(props: {
     const dateList = [...props.dateMap.keys()].sort((date1, date2) => date2.valueOf() - date1.valueOf());
     const selectedItems = props.dateMap.get(selectedDate);
     return (
-        <div style={{marginLeft: 10}}>
+        <div style={{marginLeft: 10, width: "90%"}}>
             <p>Previous shopping lists({props.dateMap.size})</p>
-            <div style={{display: "flex", flexDirection: "row"}}>
+            <div id="previous-lists-wrapper">
                 <div>
-                    <ol className="list-group list-group-numbered" style={{maxHeight: "80vh", maxWidth: 300, overflowY:"auto"}}>
+                    <ol id="previous-lists" className="list-group list-group-numbered">
                         { dateList.map(date =>
                             <ListDisplay date={date} items={props.dateMap.get(date) || []}
                                 key={date} selected={date === selectedDate} setSelectedDate={setSelectedDate} readOnly={true}/> )}
                     </ol>
                 </div>
-                <div style={{marginLeft: 10, maxWidth: 400}}>
+                <div id="previous-list-one">
                     { (selectedDate !== 0 && selectedItems !== undefined) && <ItemList list={selectedItems} copyList={true}/> }
                 </div>
             </div>
