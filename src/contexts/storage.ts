c70aaee3735c9a88e53321e28990c258965a0ed3
Dennis Francis <dennisfrancis.in@gmail.com>
2021-11-06 12:16:42 +0530

    eslint: fix errors in contexts/storage.ts
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- src/contexts/storage.ts
+++ src/contexts/storage.ts
@@ -1,96 +1,105 @@
-/* eslint-disable */
-import React from 'react'
+import React from 'react';
 import { Item } from '../types/item';
 import { ShoppingDatabase } from '../storage/storageDefs';
 
+// eslint-disable-next-line no-unused-vars
+type ListenerType = (x: Item[]) => void;
+
 export class StorageType {
-    private db: ShoppingDatabase | undefined = undefined;
-    private listeners = new Set<((x: Item[]) => void)>();
-    constructor(db?: ShoppingDatabase) {
-        this.db = db;
-        this.invokeListeners = this.invokeListeners.bind(this);
+  private db: ShoppingDatabase | undefined = undefined;
+
+  private listeners = new Set<ListenerType>();
+
+  constructor(db?: ShoppingDatabase) {
+    this.db = db;
+    this.invokeListeners = this.invokeListeners.bind(this);
+  }
+
+  public setDB(db: ShoppingDatabase): void {
+    this.db = db;
+  }
+
+  public hasDB(): boolean {
+    return !!this.db;
+  }
+
+  private invokeListeners(items: Item[]): void {
+    // console.log('Invoking ' + this.masterListeners.size + ' master listeners.');
+    this.listeners.forEach((listener) => {
+      listener(items);
+    });
+  }
+
+  public fetch(): void {
+    if (!this.db) {
+      return;
     }
 
-    public setDB(db: ShoppingDatabase) {
-        this.db = db;
+    this.db.getAllItems().then(this.invokeListeners);
+  }
+
+  public addListener(listener: ListenerType): void {
+    this.listeners.add(listener);
+  }
+
+  public removeListener(listener: ListenerType): void {
+    this.listeners.delete(listener);
+  }
+
+  public addUpdate(item: Item): void {
+    if (!this.db) {
+      return;
     }
 
-    public hasDB(): boolean {
-        return !!this.db;
+    this.db.addUpdateItem(item);
+  }
+
+  public delete(item: Item): void {
+    if (!this.db) {
+      return;
     }
 
-    private invokeListeners(items: Item[]) {
-        // console.log('Invoking ' + this.masterListeners.size + ' master listeners.');
-        this.listeners.forEach((listener) => {
-            listener(items);
-        });
+    this.db.deleteItem(item);
+  }
+
+  public clearUnsaved(): Promise<undefined> {
+    if (!this.db) {
+      return new Promise<undefined>(() => undefined);
     }
 
-    public fetch() {
-        if (!this.db)
-            return;
+    return this.db.clearUnsaved();
+  }
 
-        this.db.getAllItems().then(this.invokeListeners);
+  public saveUnsaved(saveDate: Date): Promise<undefined> {
+    if (!this.db) {
+      return new Promise<undefined>(() => undefined);
     }
 
-    public addListener(listener: (x: Item[]) => void) {
-        this.listeners.add(listener);
+    return this.db.saveUnsaved(saveDate);
+  }
+
+  public async exportToJSONText(): Promise<string> {
+    let obj;
+    try {
+      obj = await this.db?.exportToJSON();
+    } catch (e) {
+      return '';
     }
 
-    public removeListener(listener: (x: Item[]) => void) {
-        this.listeners.delete(listener);
+    return JSON.stringify(obj);
+  }
+
+  public async importFromJSONText(jsonText: string): Promise<boolean> {
+    let res = false;
+    try {
+      res = !!await this.db?.importFromJSON(jsonText);
+    } catch (e) {
+      return false;
     }
 
-    public addUpdate(item: Item) {
-        if (!this.db)
-            return;
-
-        this.db.addUpdateItem(item);
-    }
-
-    public delete(item: Item) {
-        if (!this.db)
-            return;
-
-        this.db.deleteItem(item);
-    }
-
-    public clearUnsaved() {
-        if (!this.db)
-            return;
-
-        return this.db.clearUnsaved();
-    }
-
-    public saveUnsaved(saveDate: Date) {
-        if (!this.db)
-            return;
-
-        return this.db.saveUnsaved(saveDate);
-    }
-
-    public async exportToJSONText() {
-        let obj;
-        try {
-            obj = await this.db?.exportToJSON();
-        } catch (e) {
-            return '';
-        }
-
-        return JSON.stringify(obj);
-    }
-
-    public async importFromJSONText(jsonText: string) {
-        let res = false;
-        try {
-            res = !! await this.db?.importFromJSON(jsonText);
-        } catch (e) {
-            return false;
-        }
-
-        return res;
-    }
-};
+    return res;
+  }
+}
 
 export const StorageContext = React.createContext({} as StorageType);
 export const StorageProvider = StorageContext.Provider;
