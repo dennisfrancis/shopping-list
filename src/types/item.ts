f1790f375d0e469cec3a6147bd5344c24af34880
Dennis Francis <dennisfrancis.in@gmail.com>
2021-11-20 21:28:11 +0530

    eslint: fix errors in types/item.ts
    Signed-off-by: Dennis Francis <dennisfrancis.in@gmail.com>

--- src/types/item.ts
+++ src/types/item.ts
@@ -1,78 +1,92 @@
-/* eslint-disable */
+/* eslint-disable no-unused-vars */
 export type BooleanNumber = 0 | 1;
 
 export type Item = {
-    name: string;
-    quantity: number;
-    unit: string;
-    comment: string;
-    date: Date;
-    saved: BooleanNumber;
-    category: string | undefined;
+  name: string;
+  quantity: number;
+  unit: string;
+  comment: string;
+  date: Date;
+  saved: BooleanNumber;
+  category: string | undefined;
 };
 
 export type ItemStatesAndSetters = {
-    name: string;
-    setName: (name: string) => void;
-    quantity: number;
-    setQuantity: (qty: number) => void;
-    unit: string;
-    setUnit: (unit: string) => void;
-    comment: string;
-    setComment: (comment: string) => void;
-    date: Date;
-    setDate: (date: Date) => void;
-    existing: boolean;
-    setExisting: (val: boolean) => void;
-    category: string | undefined;
-    setCategory: (category: string | undefined) => void;
+  name: string;
+  setName: (name: string) => void;
+  quantity: number;
+  setQuantity: (qty: number) => void;
+  unit: string;
+  setUnit: (unit: string) => void;
+  comment: string;
+  setComment: (comment: string) => void;
+  date: Date;
+  setDate: (date: Date) => void;
+  existing: boolean;
+  setExisting: (val: boolean) => void;
+  category: string | undefined;
+  setCategory: (category: string | undefined) => void;
 }
 
 export function cloneItem(item: Item): Item {
-    return {
-        name: item.name,
-        quantity: item.quantity,
-        unit: item.unit,
-        comment: item.comment,
-        date: item.date,
-        saved: item.saved,
-        category: item.category
-    };
+  return {
+    name: item.name,
+    quantity: item.quantity,
+    unit: item.unit,
+    comment: item.comment,
+    date: item.date,
+    saved: item.saved,
+    category: item.category,
+  };
 }
 
 export type JSONRepresentationType = {
-    items: Item[];
-    name: string;
-    message: string;
+  items: Item[];
+  name: string;
+  message: string;
 }
 
-export function getItemFromObject(obj: any): Item | undefined {
-    if (typeof obj !== 'object')
-        return undefined;
+export type ItemLikeObject = {
+  name?: unknown;
+  quantity?: unknown;
+  unit?: unknown;
+  comment?: unknown;
+  date?: unknown;
+  saved?: unknown;
+  category?: unknown;
+}
 
-    if (typeof obj.name !== 'string' ||
-        typeof obj.quantity !== 'number' ||
-        typeof obj.unit !== 'string' ||
-        typeof obj.comment !== 'string' ||
-        typeof obj.date !== 'string' ||
-        typeof obj.saved !== 'number' ||
-        (typeof obj.category !== 'string' && typeof obj.category !== 'undefined'))
-        return undefined;
+export function getItemFromObject(objSrc: unknown): Item | undefined {
+  if (typeof objSrc !== 'object') {
+    return undefined;
+  }
 
-    return {
-        name: obj.name,
-        quantity: obj.quantity,
-        unit: obj.unit,
-        comment: obj.comment,
-        date: new Date(obj.date),
-        saved: obj.saved,
-        category: obj.category,
-    }
+  const obj = objSrc as ItemLikeObject;
+
+  if (typeof obj.name !== 'string' ||
+    typeof obj.quantity !== 'number' ||
+    typeof obj.unit !== 'string' ||
+    typeof obj.comment !== 'string' ||
+    typeof obj.date !== 'string' ||
+    typeof obj.saved !== 'number' ||
+    (typeof obj.category !== 'string' && typeof obj.category !== 'undefined')) {
+    return undefined;
+  }
+
+  return {
+    name: obj.name,
+    quantity: obj.quantity,
+    unit: obj.unit,
+    comment: obj.comment,
+    date: new Date(obj.date),
+    saved: obj.saved as BooleanNumber,
+    category: obj.category,
+  };
 }
 
 export interface StorageMin {
-    clear(): void;
-    getItem(key: string): string | null;
-    removeItem(key: string): void;
-    setItem(key: string, value: string): void;
+  clear(): void;
+  getItem(key: string): string | null;
+  removeItem(key: string): void;
+  setItem(key: string, value: string): void;
 }
